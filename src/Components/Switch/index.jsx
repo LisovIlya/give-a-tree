9c794ce33998d9d53ac7f3d8f9687010452811ef
Ilia Lisov <ilya.lisov.22@gmail.com>
2023-02-03 15:07:38 +0400

    animation

--- src/Components/Switch/index.jsx
+++ src/Components/Switch/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './style.module.scss';
 import { NavLink } from 'react-router-dom';
 
 function Switch() {
+    const [togglePosition, setTogglePosition] = useState('left')
+    console.log(togglePosition)
+
     const routes = {
         main: '/give-a-tree/dist/',
         catalog: '/give-a-tree/dist/catalog',
@@ -10,14 +13,22 @@ function Switch() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.navList}>
-                <NavLink to={routes.main} className={({isActive}) => isActive ? `${styles.navItem} ${styles.active}` : styles.navItem}>
+                <NavLink
+                    onClick={() => setTogglePosition('left')}
+                    to={routes.main}
+                    className={({isActive}) => isActive ? `${styles.navItem} ${styles.active}` : styles.navItem}
+                >
                     <div className={styles.textItem}>Главная</div>
                 </NavLink>
-                <NavLink to={routes.catalog} className={({isActive}) => isActive ? `${styles.navItem} ${styles.active}` : styles.navItem}>
+                <NavLink
+                    onClick={() => setTogglePosition('right')}
+                    to={routes.catalog} 
+                    className={({isActive}) => isActive ? `${styles.navItem} ${styles.active}` : styles.navItem}
+                >
                     <div className={styles.textItem}>Каталог</div>
                 </NavLink>
-                <div className={styles.toggle}></div>
             </div>
+            <div className={[styles.toggle, styles[togglePosition]].join(' ')}></div>
         </div>
     )
 }
