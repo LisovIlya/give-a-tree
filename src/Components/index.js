ce7fe08de582b8fc181d7ba732f4234d26eb0334
Ilia Lisov <ilya.lisov.22@gmail.com>
2023-02-10 02:38:32 +0400

    first section

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,5 +2,7 @@ import Header from './Header'
 import Switch from './Switch'
 import NavIcon from './NavIcon'
 import Svg from './Svg'
+import Bookmark from './Bookmark'
+import Menu from './Menu'
 
-export { Header, Switch, NavIcon, Svg };
+export { Header, Switch, NavIcon, Svg, Bookmark, Menu };
--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './style.module.scss';
-import {Svg} from '../../Components'
+import {Svg, Bookmark, Menu} from '../../Components'
 import variables from '../../main.scss'
 
 
@@ -17,7 +17,14 @@ function Main(events) {
           <div className={styles.container}>
             <div className={styles.description}>
               <h1 className={styles.title}>Подари дерево</h1>
-              <p className={styles.descriptionText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, laboriosam. Nostrum vitae sapiente veritatis, fugit velit tempore et excepturi error odit fuga delectus, repudiandae veniam, at blanditiis iste deleniti libero!</p>
+              <p className={styles.descriptionText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className={styles.descriptionInfo}>
+              <div className={styles.leftSide}>
+                <Bookmark icon="people" height="80" count="15" description="Человек приняли участие"/>
+                <Bookmark icon="tree2" height="80" count="75" description="Деревьев посажено"/>
+              </div>
+              <div className={styles.mainButton}>Подарить</div>
             </div>
           </div>
         </section>
@@ -27,6 +34,9 @@ function Main(events) {
         <section className={styles.section}>
           <div className={styles.container}>Main-page-3</div>
         </section>
+        <div className={styles.menu}>
+          <Menu />
+        </div>
     </div>
   )
 }
