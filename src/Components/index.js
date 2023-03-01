d4cdfa2ce784097fa8fbfa48a6b89366b6de6735
Ilya <104141230+LisovIlya@users.noreply.github.com>
2023-03-02 03:13:27 +0400

    Delete src directory

--- src/Components/index.js
+++
@@ -1,9 +0,0 @@
-import Header from './Header'
-import Switch from './Switch'
-import NavIcon from './NavIcon'
-import Svg from './Svg'
-import Sort from './Sort'
-import Item from './Item'
-import Filter from './Filter'
-
-export { Header, Switch, NavIcon, Svg, Sort, Item, Filter };
